036d854e48550bf8c40d0fa048b419ecafc96a84
Adelina <154084420+adelina-chachanidze@users.noreply.github.com>
2024-08-27 21:09:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomDiceOne = "images/dice" + randomNumber1 + ".png";
+var randomDiceTwo = "images/dice" + randomNumber2 + ".png";
+
+var diceOne = document.querySelectorAll("img") [0];
+diceOne.setAttribute ("src", randomDiceOne);
+
+var diceTwo = document.querySelectorAll("img") [1];
+diceTwo.setAttribute ("src", randomDiceTwo);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector ("h1").innerHTML = "Player 1 Wins!";
+    }
+    else if
+    (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";   
+    }
+    else {document.querySelector("h1").innerHTML = "It's a Draw!";}
